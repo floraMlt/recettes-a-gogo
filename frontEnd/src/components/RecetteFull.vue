d58f9becc573941c752153a90da7dfb44780e721
Mallet Flora <iciflaurore33@orange.fr>
2020-10-05 15:03:59 +0200

    Css page Recette détails

--- frontEnd/src/components/RecetteFull.vue
+++ frontEnd/src/components/RecetteFull.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="recetteFull">
-      <h2> {{this.recetteDetail.title}} </h2>
-      <div id="divImg">
-        <img id="img" :alt="title" :src="img">
+      <div class="titleLike">
+        <h2 class="title"> {{this.recetteDetail.title}} </h2>
+        <i id = "likeHeart" v-on:click="checkLike" :class="{'heart icon': liked, 'heart outline icon': !liked}"></i>
       </div>
-      <i v-on:click="checkLike" :class="{'heart icon': liked, 'heart outline icon': !liked}"></i>
-      <p> {{this.recetteDetail.txt}} </p>
+      <!--<img class="img" :alt="title" :src="img">-->
+      <div class="imgIngredients">
+        <img class="img" :alt="title" src="/image/crepes.jpg">
+        <div class="ingredients">
+          <p class="titleIngredient">Ingrédients</p>
+          <ul class="listIngredients">
+            <!-- <div v-for="ingredient in ingredients" :key="recette.ingredients" class="listIngredients">
+                <p :ingr="ingredient"/>
+            </div> -->
+            <li>2 avocats (pelés et avec les noyaux retirés)</li>
+            <li>le jus d'un citron</li>
+            <li>¼ de concombre, coupé grossièrement</li>
+            <li>1 petite tomate, coupée</li>
+          </ul>
+        </div>
+      </div>
+      <p class="txt"> {{this.recetteDetail.txt}} </p>
       <div class="comments" v-if="comments">
         <p> {{this.recetteDetail.comments}} </p>
       </div>
@@ -37,5 +52,60 @@ export default {
 </script>
 
 <style scoped>
-
+li {
+  list-style-type: circle;
+}
+.title {
+  font-size: 3em;
+  font-weight: lighter;
+  height:1em;
+  font-family: "VladimirScript";
+  margin: 0;
+}
+.titleLike {
+  display : flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 3%;
+}
+#likeHeart {
+  margin-top: 2.5%;
+  margin-left: 2.5%;
+  margin-right: 0;
+}
+.imgIngredients {
+  display:flex;
+  justify-content: center;
+  align-items: center;
+}
+.img {
+  object-fit: cover;
+  max-width: 100%;
+  max-height: 100%;
+  border-radius: 8px;
+}
+.ingredients {
+  text-align: left;
+  color: black;
+}
+.titleIngredient{
+ text-align: center;
+ font-size: 1.5em;
+ font-weight: bold;
+   color: #554D4B;
+}
+.txt {
+  background-color: #F2F2F2;
+  border: #554d4b3d 0.1px solid;
+  border-radius: 3px;
+  width: 70%;
+  display: flex;
+  justify-content: center;
+}
+.recetteFull {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: ;
+}
 </style>
--- frontEnd/src/views/PageRecette.vue
+++ frontEnd/src/views/PageRecette.vue
@@ -1,22 +1,33 @@
 <template>
-    <div class="whiteRect">
-      <div class="DetailRecipe">
-          <!-- <RecetteFull :title="this.recette.title" :img="this.recette.img" :like="this.recette.like" :txt="this.recette.txt" :comments="this.recette.comments"/> -->
-        <RecetteFull :recetteDetail="this.recette"/>
+    <div class="PageRecette">
+      <div class="titleDeco">
+        <h1 id="logo"> Recettes à gogo </h1>
+        <Deconnexion class="deconnect"/>
       </div>
-      <!-- <p>{{ this.$route.params.id }}</p> -->
+      <BarreNav/>
+      <div class="whiteRect">
+        <div class="DetailRecipe">
+            <!-- <RecetteFull :title="this.recette.title" :img="this.recette.img" :like="this.recette.like" :txt="this.recette.txt" :comments="this.recette.comments"/> -->
+          <RecetteFull :recetteDetail="this.recette"/>
+        </div>
+        <!-- <p>{{ this.$route.params.id }}</p> -->
+      </div>
     </div>
 </template>
 
 <script>
 import RecetteFull from '@/components/RecetteFull.vue'
+import BarreNav from '@/components/BarreNav.vue'
+import Deconnexion from '@/components/Deconnexion.vue'
 import RecetteApi from '@/services/api/recettes.js'
 import CookieService from '@/services/cookies/cookies.js'
 
 export default {
   name: 'PageRecette',
   components: {
-    RecetteFull
+    RecetteFull,
+    BarreNav,
+    Deconnexion
   },
   data(){
     return {
@@ -46,11 +57,38 @@ export default {
 </script>
 
 <style>
-
-.allRecipes{
+.whiteRect {
+  background:white;
+  width:60%;
+  border-radius: 3%;
+  margin-left:auto;
+  margin-right: auto;
+  padding: 2% 0;
+}
+h1 {
+  font-family: "VladimirScript";
+  font-size: 4em;
+  font-weight: lighter;
+  color: #554D4B;
+  margin: 0;
+}
+.titleDeco {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 3%;
+}
+.deconnect {
+  position: relative;
+  margin-top: 1.2%;
+  z-index: 2;
+}
+#logo {
+  margin-left: 13%;
+}
+/*.allRecipes{
   margin-top:10%;
 }
-
 .whiteRect {
   background:#fff9f0;
   width:60%;
@@ -58,6 +96,5 @@ export default {
   flex-wrap:wrap;
   margin-left:auto;
   margin-right: auto;
-}
-
+}*/
 </style>
